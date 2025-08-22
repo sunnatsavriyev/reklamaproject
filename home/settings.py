31c1236ed27f6a18f516a2d4a2376a9067ce6c5a
Sunnat Savriyev <sunnatchiksavriyev@gmail.com>
2025-08-22 13:52:53 +0500

    changed jwt time

--- home/settings.py
+++ home/settings.py
@@ -129,7 +129,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
