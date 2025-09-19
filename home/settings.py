b43399d553a3edb7f1f06b581e51939c01753519
Sunnat Savriyev <sunnatchiksavriyev@gmail.com>
2025-09-19 09:29:25 +0500

    edited settings3

--- home/settings.py
+++ home/settings.py
@@ -133,7 +133,7 @@ AUTHENTICATION_BACKENDS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
